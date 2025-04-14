34b0f47c5a817db0a6ab08a6708ef359fe8d7b5c
rito2002 <trieger2002@gmail.com>
2025-04-14 19:46:48 +0200

    Umami nur in Prod

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,14 @@
 </script>
 
 <svelte:head>
-	<script
-		async
-		defer
-		data-website-id="95338bbf-306c-4aaa-ba2a-37815e195194"
-		src="https://umami.rieger-systems.eu/script.js"
-	></script>
+	{#if import.meta.env.PROD}
+		<script
+			async
+			defer
+			data-website-id="95338bbf-306c-4aaa-ba2a-37815e195194"
+			src="https://umami.rieger-systems.eu/script.js"
+		></script>
+	{/if}
 	<title>Rieger Systems</title>
 	<meta name="description" content="Technologie, die trägt. Für Menschen. Mit Verantwortung." />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
