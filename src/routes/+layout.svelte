36d6b55bc7783f22a08be0cf90fae663a94e5be5
rito2002 <trieger2002@gmail.com>
2025-04-14 14:39:07 +0200

    Umami hinzugefügt

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 		async
 		defer
 		data-website-id="95338bbf-306c-4aaa-ba2a-37815e195194"
-		src="https://umami.rieger-systems.eu/umami.js"
+		src="https://umami.rieger-systems.eu/script.js"
 	></script>
 	<title>Rieger Systems</title>
 	<meta name="description" content="Technologie, die trägt. Für Menschen. Mit Verantwortung." />
