d54ccd8a1e84251e1974b9469eeb511072409cef
rito2002 <trieger2002@gmail.com>
2025-05-01 11:07:25 +0200

    Default Layout finalisiert

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="min-h-screen flex flex-col bg-base-100 text-base-content">
     <Header />
-    <main class="flex-grow">
+
+    <main id="main" class="flex-grow">
       <NuxtPage />
     </main>
+
     <Footer />
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import Header from "~/components/navigation/Header.vue";
 import Footer from "~/components/navigation/Footer.vue";
 </script>
