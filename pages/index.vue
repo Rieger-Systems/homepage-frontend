777d0b8a8c7f85e7b7be162759abcd5c1da2c44e
rito2002 <trieger2002@gmail.com>
2025-05-01 14:33:16 +0200

    Mini-Mission entfernt von Startseite

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="bg-base-100 text-base-content">
     <HeroSection />
-    <MissionShort />
     <ServicesOverview />
     <ProjectShowcase />
     <Differentiation />
@@ -11,7 +10,6 @@
 
 <script setup lang="ts">
 import HeroSection from "~/components/sections/homepage/HeroSection.vue";
-import MissionShort from "~/components/sections/homepage/MissionShort.vue";
 import ServicesOverview from "~/components/sections/homepage/ServicesOverview.vue";
 import ProjectShowcase from "~/components/sections/homepage/ProjectShowcase.vue";
 import Differentiation from "~/components/sections/homepage/Differentiation.vue";
